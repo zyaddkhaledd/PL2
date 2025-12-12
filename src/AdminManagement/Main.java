784c76db70d191c971289a61120fb58fec7bab85
Zyad Khaled <zyadk9990@gmail.com>
2025-12-12 16:06:20 +0200

    zzzzzzzzz

--- src/AdminManagement/Main.java
+++ src/AdminManagement/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package pl2;
+package AdminManagement;
 
 /**
  *
@@ -10,7 +10,7 @@ package pl2;
  */
 import java.util.Scanner;
 
-public class PL2 {
+public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
